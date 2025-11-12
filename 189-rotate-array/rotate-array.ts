1f1dcab28e1168e22128e903beb8a7d8d57547fa
sahilleap <138430303+sahilleap@users.noreply.github.com>
2025-11-13 01:21:48 +0530

    Time: 776 ms (17.93%) | Memory: 67 MB (43.59%) - LeetSync

--- 189-rotate-array/rotate-array.ts
+++ 189-rotate-array/rotate-array.ts
@@ -3,6 +3,7 @@
  */
 function rotate(nums: number[], k: number): void {
     for (let i = 0; i < k; i++) {
-        nums.unshift(nums.pop())
+        nums.unshift(nums[nums.length -1])
+        nums.pop()
     }
 };
